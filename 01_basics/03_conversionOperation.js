e3e53553c221b5944dda891432f8b2a2d36af38f
Anisha22071993 <arieldancestudio1925@gmail.com>
2025-12-05 12:43:44 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33" // 33 
+let score = "33abc" // NaN
+let score = null // 0
+let score = undefined // NaN
+let score = true // 1   false =0
+let score = "anisha" //NaN
+
+//console.log (typeof score);
+//console.log (typeof (score));
+
+let valueInNumber = Number(score)
+//console.log (typeof valueInNumber);
+//console.log (valueInNumber);
+//let is isLoggedIn = 1
+
+//let booleanIsLoggedIn = Boolean (isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "anisha" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber); // 33
+console.log(typeof stringNumber);  // 33 string
