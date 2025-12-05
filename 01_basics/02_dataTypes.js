d735e0f9e4945d827a48c7961a1f794f6f7b02b7
Anisha22071993 <arieldancestudio1925@gmail.com>
2025-12-05 11:51:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+console.log (3
+    +
+    3) //code readability should be high
+
+console.log ("Anisha")
+
+let name = "anisha"
+let age = 31
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint =>  big number
+// string => ""
+// boolean => true/false
+// null => standalone value (defined emptyness)
+// undefined => not defined value
+// symbol => unique
+
+// object
+
+console.log(typeof "anisha"); // string
+console.log (typeof null); // Object
+console.log (typeof undefined); // undefined 
+     
+
