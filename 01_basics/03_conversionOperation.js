9babe7903519a238a983364415266f00a122862a
Anisha22071993 <arieldancestudio1925@gmail.com>
2025-12-06 11:04:38 +0000

    operation of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,12 @@
+/* 
 let score = "33" // 33 
 let score = "33abc" // NaN
 let score = null // 0
 let score = undefined // NaN
 let score = true // 1   false =0
 let score = "anisha" //NaN
+*/
+let score = "33abc"
 
 //console.log (typeof score);
 //console.log (typeof (score));
@@ -11,7 +14,8 @@ let score = "anisha" //NaN
 let valueInNumber = Number(score)
 //console.log (typeof valueInNumber);
 //console.log (valueInNumber);
-//let is isLoggedIn = 1
+
+//let is isLoggedIn = "anisha"
 
 //let booleanIsLoggedIn = Boolean (isLoggedIn)
 //console.log(booleanIsLoggedIn);
@@ -23,5 +27,44 @@ let valueInNumber = Number(score)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber); // 33
-console.log(typeof stringNumber);  // 33 string
+//console.log(stringNumber); // 33
+//console.log(typeof stringNumber);  // 33 string
+
+// ******************************Operations**********************************
+
+let value = 3
+let negValue = -value
+//console.log (negValue);
+
+// console.log (2+2);
+// console.log (2-2);
+// console.log (2*2);
+// console.log (2**3);
+// console.log (2/3);
+// console.log (2%3);
+
+let str1 = "hello"
+let str2 = " anisha"
+
+let str3 = str1 + str2
+//console.log (str3);
+
+//console.log ("1" + 2);
+//console.log (1 + "2");
+//console.log ("1" + 2 + 2);
+//console.log (1 + 2 + "2");
+
+//console.log ((3 + 4) * 5 % 3);
+
+//console.log (+true);
+//console.log (+"");
+
+let num1, num2, num3 
+num1 = num2 = num3 
+
+let gameCounter = 100
+++gameCounter;
+console.log (gameCounter);
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
