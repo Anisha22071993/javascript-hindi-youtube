3540f2a8f6120288f2ef831a58399f4180a9e24d
Anisha22071993 <arieldancestudio1925@gmail.com>
2025-12-11 12:00:56 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,57 @@
+// var c = 300
+let a = 300
+if (true) {
+ let a = 10
+const b = 20
+var c = 30
+console.log ("INNER: ", a);
+}
+
+//for (let i = 0; i < array.length; i++) {
+  //  const element = array [i];
+//}
+
+//console.log (a);
+//console.log (b);
+//console.log (c);
+
+function one (){
+    const username = "anisha"
+
+    function two (){
+        const website = "youtube"
+        console.log (username);
+    }
+    // console.log (website);
+
+    two()
+}
+
+//one()
+
+if (true) {
+    const username = "anisha"
+    if (username === "anisha") {
+        const website = "youtube"
+        console.log (username + website);
+    }
+    // console.log (website);
+}
+
+// console.log (username);
+
+// ++++++++++++++++++ interesting +++++++++++++++++++ 
+
+console.log (addone(5))  // correct in this format
+
+function addone (num){
+    return num + 1
+}
+
+
+addTwo (5) // inncorrect in this format (before declaration)
+
+const addTwo = function (num) {
+    return num + 2
+}
+
